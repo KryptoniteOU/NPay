5d5142935f3f933fb384b4c9afb69e204366969c
pubill <33612994+pubill@users.noreply.github.com>
2018-11-06 18:47:50 +0900

    Update bin/etc/installMN.sh

--- bin/etc/installMN.sh
+++ bin/etc/installMN.sh
@@ -36,13 +36,13 @@ if [[ $DOSETUP =~ "y" ]] ; then
   sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 
   cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  sudo fallocate -l 2G /swapfile
+  sudo chown root:root /swapfile
+  sudo chmod 0600 /swapfile
+  sudo bash -c "echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
+  sudo mkswap /swapfile
+  sudo swapon /swapfile
+  sudo echo '/swapfile none swap sw 0 0' >> /etc/fstab
   cd
 
   wget http://downloads.npay.network/linux/linux.zip
